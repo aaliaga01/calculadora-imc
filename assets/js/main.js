368db7c1f56572aee2b551ff7748913a3b602f77
KarinaLaroze <karinalaroze@gmail.com>
2017-05-08 13:43:47 -0300

    quitando comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 function calculaIMC(){
-  //declaramos las variables
+
   var peso, altura, imc;
   peso=document.getElementById("peso").value;
   altura=document.getElementById("altura").value;
-  //comprobamos que los campos
-  //no vengan vacíos
+ 
   if(peso!="" && altura!=""){
     //aplicamos la fórmula
     altura = parseInt(altura)/100;
